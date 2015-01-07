ea45008c15695adf0c8e557a227d2c37c27eeb5d
mikekavouras <kavourasm+ed-1@gmail.com>
2015-01-07 12:35:43 -0500

    add comment and overlay

--- The Math/JournalViewController.swift
+++ The Math/JournalViewController.swift
@@ -23,10 +23,13 @@ class JournalViewController: UIViewController, CategoryViewDelegate {
     @IBOutlet weak var loveView: LoveView!
     
     @IBOutlet weak var moodDescriptionView: UIView!
+    @IBOutlet weak var overlayView: UIView!
     
     @IBOutlet weak var moodImageView: UIImageView!
     @IBOutlet weak var moodDescriptionLabel: UILabel!
     @IBOutlet weak var categoryLabel: UILabel!
+    
+    @IBOutlet weak var commentViewTopConstraint: NSLayoutConstraint!
     
     var delegate: JournalViewControllerDelegate?
     
@@ -39,6 +42,45 @@ class JournalViewController: UIViewController, CategoryViewDelegate {
         healthView.delegate = self
         workView.delegate = self
         loveView.delegate = self
+    }
+    
+    @IBAction func overlayButtonTapped(sender: AnyObject) {
+        hideOpportityToAddDetails()
+    }
+    
+    private func presentOpportunityToAddDetails() {
+        commentViewTopConstraint.constant = 20
+        UIView.animateWithDuration(0.6, delay: 0.1, usingSpringWithDamping: 0.9, initialSpringVelocity: 1.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+            
+            self.view.layoutIfNeeded()
+            self.overlayView.alpha = 1.0
+            
+            }) { (done: Bool) -> Void in
+                
+                UIView.animateWithDuration(0.2, animations: {
+                    self.moodDescriptionView.alpha = 0.0
+                })
+        }
+    }
+    
+    private func hideOpportityToAddDetails() {
+        commentViewTopConstraint.constant = -181
+        UIView.animateWithDuration(0.6, delay: 0.1, usingSpringWithDamping: 0.9, initialSpringVelocity: 1.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            
+            self.view.layoutIfNeeded()
+            self.overlayView.alpha = 0.0
+            
+            }) { (done: Bool) -> Void in
+                
+                self.delegate?.didEndEditingMood()
+                return()
+        }
+
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.3, target: self, selector: Selector("cleanup"), userInfo: nil, repeats: false)
+    }
+    
+    func cleanup() {
+        delegate?.didEndEditingMood()
     }
     
     
@@ -58,9 +100,6 @@ class JournalViewController: UIViewController, CategoryViewDelegate {
     }
     
     func didEndMoodChangeForCategory(category: Category) {
-//        delegate?.didEndEditingMood()
-        UIView.animateWithDuration(0.2, animations: {
-            self.moodDescriptionView.alpha = 0.0
-        })
+        presentOpportunityToAddDetails()
     }
 }
