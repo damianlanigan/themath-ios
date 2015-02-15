f6f1670f7e84f573781397c7d1258fcf4138076b
mikekavouras <kavourasm@gmail.com>
2015-02-14 20:16:43 -0500

    meh

--- The Math/OnboardingViewController.swift
+++ The Math/OnboardingViewController.swift
@@ -12,7 +12,7 @@ protocol OnboardingViewControllerDelegate: class {
     func didFinishOnboarding(viewController: OnboardingViewController)
 }
 
-class OnboardingViewController: GAITrackedViewController,
+class OnboardingViewController: GAITrackedViewController,  
     CategorySelectionViewControllerDelegate,
     UIScrollViewDelegate,
     AuthViewControllerDelegate {
