0d82bd1358c806f0e67081e65d61442ae42e0a29
mikekavouras <kavourasm@gmail.com>
2015-05-16 15:18:12 -0400

    web view controller

---
+++ The Math/WebViewController.swift
@@ -0,0 +1,37 @@
+//
+//  WebViewController.swift
+//  HowAmIDoing
+//
+//  Created by Michael Kavouras on 5/16/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
