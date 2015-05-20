b5c25d7fdf32deece32e3a13bb4d0ef801203a79
mikekavouras <kavourasm@gmail.com>
2015-05-20 14:50:15 -0400

    added those things hopefully

---
+++ The Math/ChartFooterLabel.swift
@@ -0,0 +1,21 @@
+//
+//  ChartFooterLabel.swift
+//  HowAmIDoing
+//
+//  Created by Michael Kavouras on 5/20/15.
+//  Copyright (c) 2015 Mike Kavouras. All rights reserved.
+//
+
+import UIKit
+
+class ChartFooterLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
